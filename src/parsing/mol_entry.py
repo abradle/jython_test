11c638f06d0a53925f0606a959d703ae93f5ac39
A B <anthony.richard.bradley@gmail.com>
2015-01-21 17:21:01 +0000

    Done a little more

--- src/parsing/mol_entry.py
+++ src/parsing/mol_entry.py
@@ -10,15 +10,18 @@ def make_request():
 
 def check_stream_type(in_stream):
     """Function to check what the input is"""
+    # File types, SMILES, SD, MOL, INCHI
     print "Checking stream"
-
+    # First check - is this a file, or a stream
+    # If it is a file, then we want to convert to stream, everrything should be stream
+    print "Checking type"
+    # Now check what file format we have
+ 
 
 class ReadStream():
      """Class to read stream given an input field"""
-
-
 #We're having to dummy this here - as the request object is passed in by the calling function
-request.body = make_request()
+request = make_request()
 
 # Now lets test what kind of functon is required
 my_mols = SmilesMolSupplier('test.smi')
