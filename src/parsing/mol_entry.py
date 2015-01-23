dd8d2779b6db7c24448bb56b56f496cea927c8e0
A B <anthony.richard.bradley@gmail.com>
2015-01-23 17:28:15 +0000

    Added some simple molecule parsing

--- src/parsing/mol_entry.py
+++ src/parsing/mol_entry.py
@@ -1,11 +1,22 @@
 from java import lang
 lang.System.loadLibrary('GraphMolWrap')
 from org.RDKit import *
+from threading import Thread
+import os
+
+
+class Request():
+   """Class to hold requests to dummy the input behaviour"""
+   def __init__(self):
+       print "Initialsing request"
 
 
 def make_request():
     """Function to build a request object-for testing"""
     print "Making request"
+    my_req = Request()
+    my_req.in_stream = "/jython_test/src/test_data/test.smi"
+    return my_req
 
 
 def check_stream_type(in_stream):
@@ -13,26 +24,58 @@ def check_stream_type(in_stream):
     # File types, SMILES, SD, MOL, INCHI
     print "Checking stream"
     # First check - is this a file, or a stream
+    if os.path.isfile(in_stream):
+        in_stream = open(in_stream).read()
+        print "OPENED FILE INPUT STREAM"
+    else:
+        print in_stream
+        print "READING FROM INPUT STREAM"
     # If it is a file, then we want to convert to stream, everrything should be stream
     print "Checking type"
-    # Now check what file format we have
- 
+    # Now check what file format we havea
+    my_mols = in_stream.split("$$$$")
+    if len(my_mols) > 1:
+        rdmol = RWMol.MolFromMolBlock(my_mols[0])
+        # If this is none - this may not be a MOL file so we need to test the others
+        if rdmol is None:
+            print "MAY NOT BE SDF FILE"
+        else:
+            file_flag = "sdf"
+            delim = "$$$$"
+            return file_flag, delim
 
-class ReadStream():
-     """Class to read stream given an input field"""
-#We're having to dummy this here - as the request object is passed in by the calling function
+    my_mols = in_stream.split("\n")
+    test_line = my_mols[1]
+    header = my_mols[0]
+    # Check for a comma seperatin
+    if len(header.split("\t")) > 1:
+        delim = "\t"
+    elif len(header.split(",")) > 1:
+        delim = ","
+    elif len(header.split(" ")) > 1:
+        delim = " "
+    else:
+        print "UNKNOWN DELIMTER"
+        return
+    # Check for whitespace delimerter
+    rdmol = RWMol.MolFromSmiles(test_line.split(delim)[0])
+    if rdmol:
+        file_flag = "smiles"
+        return file_flag, delim
+    elif rdmol is None:
+        pass
+    rdmol = RWMol.MolFromInchi(test_line.split(delim)[0])
+    if rdmol:
+        file_flag = "inchi"
+        return file_flag, delim
+    elif rdmol is None:
+        pass
+    print "UNKNOWN FILE TYPE"
+    return None, None
+
+# Make the request
 request = make_request()
-
-# Now lets test what kind of functon is required
-my_mols = SmilesMolSupplier('test.smi')
-
-out_mols = SDWriter("out_sd.sdf")
-
-while my_mols.atEnd() != True:
-    mol = my_mols.next()
-    num_HBA = RDKFuncs.calcNumHBA(mol)
-    logp = RDKFuncs.calcMolLogP(mol)
-    rmm = RDKFuncs.calcMolMR(mol)
-    num_HA =  mol.getNumHeavyAtoms()
-    num_HBD =  RDKFuncs.calcNumHBD(mol)
-    out_mols.write(mol)
+# Check the type
+file_flag, delim = check_stream_type(request.in_stream)
+print "FILE TYPE: ",file_flag
+print "DELIMITER: ",delim
